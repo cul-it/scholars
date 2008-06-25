c1a5ed7fd95f830f6c3630fe6b68fc8f2b6522bb
mw542 <mw542@cornell.edu>
2008-06-25 15:30:58 +0000

    Updated highlighting for inline editing

--- modifications/js/propertyGroupSwitcher.js
+++ modifications/js/propertyGroupSwitcher.js
@@ -8,14 +8,20 @@ $(document).ready(function() {
     }
     
     function switchPropertyGroup(targetGroup) {
+        $("ul#profileCats li.currentCat").removeClass("currentCat");
         $("ul#profileCats li a").removeAttr("id").each(function(){
             var thisID = $(this).attr("href");
-            if (thisID == ("#" + targetGroup)) { $(this).attr("id", "currentCat"); }
+            if (thisID == ("#" + targetGroup)) { 
+                $(this).attr("id", "currentCat");
+                $(this).parent().addClass("currentCat")
+             }
             $(thisID).hide();
         });
         $("#" + targetGroup).show();
         $("ul#dashboardNavigation h2").removeClass("active");
+        $("ul#dashboardNavigation ul.active").removeClass("active");
         $("ul#dashboardNavigation li." + targetGroup + " h2").addClass("active"); 
+        $("ul#dashboardNavigation li." + targetGroup + " ul").addClass("active"); 
     }
     
     function switchDashboardGroup(targetGroup, method) {
@@ -25,19 +31,71 @@ $(document).ready(function() {
                 if ($(this).parent().hasClass(targetGroup)) { $(this).show(); }
             }
             if (method == "slide") {
-                $(this).slideUp("fast");
-                if ($(this).parent().hasClass(targetGroup)) { $(this).slideDown(); };
+                $(this).slideUp("medium");
+                if ($(this).parent().hasClass(targetGroup)) { $(this).slideDown("slow"); };
             }
         });
     }
-    
+        
+    // the parameter for this does need a pound sign
     function changeHighlight(property) {
-        $("h4").removeClass("targeted");    
         if (property != null) {
-            $(property).children("h4").addClass("targeted");
+            removeHighlighting();
+            bgColor = "#fffbcc";
+            borderColor = "#ede9be";
+            $(property).animate({ 
+                borderBottomColor: borderColor,
+                borderTopColor: borderColor,
+                borderLeftColor: borderColor,
+                borderRightColor: borderColor,
+                backgroundColor: bgColor
+                }, 500);
+            $(property).addClass("highlighted");
         }
     }
- 
+    
+    function removeHighlighting(){
+        $("#content div.highlighted").css({ borderColor:"#fff", backgroundColor:"#fff" }).removeClass("highlighted");
+        $("ul.dashboardCategories a.highlighted").removeClass("highlighted");
+    }
+    
+    function scrollToHighlight(property){
+        var targetOffset = $(property).offset().top;
+        var divHeight = document.getElementById(property.substring(1)).offsetHeight;
+        var viewportHeight = getViewportHeight();
+        var scrollPosition = getScrollPosition();
+        // alert("Target Top: " + targetOffset + "\nDiv Height: " + divHeight + "\nViewport: " + viewportHeight + "\nScroll Position: " + scrollPosition);
+        // $('html,body').animate({scrollTop: targetOffset - (viewportHeight/2)}, 700); 
+        if (targetOffset < scrollPosition || (targetOffset + divHeight) > (viewportHeight + scrollPosition - 40)){
+            if(divHeight >=  viewportHeight*.6) { 
+                $('html,body').animate({scrollTop: targetOffset - 40}, 700); 
+            }
+            if(divHeight < viewportHeight*.6) { 
+                $('html,body').animate({scrollTop: targetOffset - (viewportHeight/2)}, 700);
+            }
+        }
+        changeHighlight(property);
+    }
+    
+    function getScrollPosition() {
+       var height = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+       return height ? height : 0;
+    }
+    
+    function getViewportHeight() {
+        if (typeof window.innerHeight != 'undefined'){
+            viewportheight = window.innerHeight
+        }
+        else if (typeof document.documentElement != 'undefined' 
+                && typeof document.documentElement.clientHeight != 'undefined' 
+                && document.documentElement.clientHeight != 0) {
+            viewportheight = document.documentElement.clientHeight
+        }
+       return viewportheight;
+    }
+
+
+
     // do the following after the page initially loads
     
         var initialGroup = $("ul#profileCats a#currentCat").attr("href").substring(1);
@@ -55,7 +113,7 @@ $(document).ready(function() {
         if ( parameter != null ) {
             var propertyID = "#" + parameter;
             var paramGroupID = $(propertyID).parent("div").attr("id");
-            changeHighlight(propertyID);
+            scrollToHighlight(propertyID);
             initialGroup = paramGroupID;
         }
     
@@ -68,29 +126,47 @@ $(document).ready(function() {
     
         switchPropertyGroup(initialGroup);
         switchDashboardGroup(initialGroup, "quick");
+
+
     
-    
-    // event handlers
+    // click handlers
     
         $("ul#profileCats li a").click(function(){
             var thisGroupID = $(this).attr("href").substring(1);
             switchPropertyGroup(thisGroupID);
             switchDashboardGroup(thisGroupID, "slide");
             changeUrlFragment(thisGroupID);
+            removeHighlighting();
+            $(this).blur();
             return false;
         });
         
         $("ul.dashboardCategories li a").click(function(){
-            var thisPropertyID = $(this).attr("href");
-            var thisGroupID = $(thisPropertyID).parent().attr("id");
-            changeHighlight(thisPropertyID);
-            switchPropertyGroup(thisGroupID);
-            changeUrlFragment(thisGroupID);
-            return false;
+            if($(this).hasClass("highlighted") == true){
+                removeHighlighting();
+                $(this).blur();
+                return false;
+            }
+            else {
+                var thisPropertyID = $(this).attr("href");
+                var thisGroupID = $(thisPropertyID).parent().attr("id");
+                switchPropertyGroup(thisGroupID);
+                changeUrlFragment(thisGroupID);
+                scrollToHighlight(thisPropertyID);
+                // changeHighlight(thisPropertyID);
+                $(this).addClass("highlighted");
+                $(this).blur();
+                return false;
+            }
+        });
+        
+        $("div.propsCategory div").not("div.propsCategory div.highlighted").click(function(){
+            removeHighlighting();
         });
     
         $("ul#dashboardNavigation li h2").click(function(){
             $(this).parent().children("ul").slideToggle("fast");
+            $(this).blur();
             return false;
         });
 });
