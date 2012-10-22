f37478eda25fa47841878f84ee71cf7fbb598da5
Brian Caruso <bdc34@cornell.edu>
2012-10-22 15:24:03 +0000

    Improvements to capture page

--- webPageCapture/capture.py
+++ webPageCapture/capture.py
@@ -13,63 +13,85 @@ from selenium import webdriver
 # make a screen shot for each URL in STDIN
 # output resulting png to STDOUT
 
-### to install pyvirtualdisplay on RHEL5:
+# ## to install pyvirtualdisplay on RHEL5:
 # sudo bash
 # curl http://python-distribute.org/distribute_setup.py | python
 # curl -k https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
 
 
+# the .png or .jpg at the end of the file sufix controls 
+# the image format of the file
+imageType = ".png"
+fileSufix = "Image" + imageType
 
 ### Functions for URL to file name conversion ###
 
-def queryToPath( queryStr ):
+def queryToPath(queryStr):
   """Convert the query string of a URL to a path"""    
-  return "/".join(  map( quote_plus, queryStr.split("&") ) )
+  return "/".join(map(quote_plus, queryStr.split("&")))
   
-def urlToFileName( url ):
+def urlToFileName(url):
   """Convert the URL and query string into a file path.
      The returned string will not start with a slash. """
   urlToPathAndQuery = re.compile(r"^http://([^\?]*)\?(.*)")
-  match = urlToPathAndQuery.match( url )
+  match = urlToPathAndQuery.match(url)
   if match is not None:
-    #query found, try this:
-    return match.groups()[0] + "/" + queryToPath( match.groups()[1])
+    # query found, try this:
+    return match.groups()[0] + "/" + queryToPath(match.groups()[1])
   else:
     urlregex = re.compile(r"^http://(.*)$")
-    match = urlregex.match( url )
+    match = urlregex.match(url)
     if match is not None:
-      return match.groups()[0] + "Image"
+      return match.groups()[0] + fileSufix
     else:
-      #not sure what it is but it might be a hostname
+      # not sure what it is but it might be a hostname
       return url
   
 
 basePath = "./webPageImages/" 
 
-display = Display(visible=0,size=(800,600))
+display = Display(visible=0, size=(800, 600))
 display.start()
 browser = webdriver.Firefox()
 
-for url in sys.stdin:
-    sys.stdout.write("url: " + url + "\n")
+# for url in sys.stdin:
+while 1:
+    #try:
+        try: 
+            url = sys.stdin.readline()
+        except KeyboardInterrupt:
+            break
+	    
+        if not url:
+            break
 
-    saveTo = basePath + urlToFileName( url )
-    dirname = os.path.dirname(saveTo)
-    filename = os.path.basename(saveTo) 
-       
-    sys.stdout.write("dir: " + dirname + "\n")
-    sys.stdout.write("filename: " + filename + "\n")
+        sys.stderr.write("web page capture url " + url)
+        saveTo = basePath + urlToFileName(url)
+        dirname = os.path.dirname(saveTo)
+        filename = os.path.basename(saveTo) 	   
+        sys.stderr.write("web page save to: " + saveTo + "\n")
+        if not os.path.exists(dirname):            
+            os.makedirs(dirname)
+        else:
+            sys.stderr.write(" dir exists")	 
+        if not os.path.exists( saveTo ):
+            try:
+                browser.get( url )
+            except Exception as e :
+                #the following line is causing an error
+                sys.stderr.write( e )
+              
+            browser.save_screenshot(saveTo)
+        else:
+            sys.stderr.write(" file exists")
 
-    if not os.path.exists(dirname) :
-        os.makedirs( dirname )
- 
-    browser.get( url )
-    browser.save_screenshot( dirname + '/' + filename + ".png")
-
-  #return image file contents as binary data
-  #sys.stdout.write(
-#	"Content-type: image/"+imageType+"\r\n\r\n" +
-#	file(imageFile,"rb").read() )
+	    # return image file contents as binary data
+        sys.stdout.write("Content-type: image/" + imageType + "\r\n\r\n" + 
+                         file(saveTo, "r").read())
+        sys.stdout.flush()
+        
+    #except Exception as ex:
+     # print "problem with " , url , " ", ex 
 
 browser.quit()
 display.stop()
