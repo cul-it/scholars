fd12a2aa6bd72bc4a1fd297a61477045b8053696
Tim Worrall <tlw72@cornell.edu>
2014-05-19 15:55:33 +0000

    more js tweaks

--- productMods/js/homePageUtils.js
+++ productMods/js/homePageUtils.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
                     });
                     var viewMore = "<ul id='viewMoreFac'><li><a href='"
                                 + urlsBase
-                                + "/people/%23http://vivoweb.org/ontology/core%23FacultyMember' alt='" 
+                                + "/people#http://vivoweb.org/ontology/core%23FacultyMember' alt='" 
                                 + i18nStrings.viewAllFaculty + "'>"
                                 + i18nStrings.viewAllString + "</a></li?</ul>";
                     $('div#research-faculty-mbrs').append(viewMore);
@@ -155,7 +155,7 @@ $(document).ready(function(){
             html += "</ul><ul style='list-style:none'>"
                     + "<li style='font-size:0.9em;text-align:right;padding: 6px 16px 0 0'><a href='" 
                     + urlsBase 
-                    + "/organizations/%23http://vivo.library.cornell.edu/ns/0.1%23ServiceLaboratory ' alt='" 
+                    + "/organizations#http://vivo.library.cornell.edu/ns/0.1%23ServiceLaboratory ' alt='" 
                     + "view all research facilities'>" 
                     + i18nStrings.viewAllString + "</a></li></ul>";
         }
