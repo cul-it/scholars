26c2ec1ad8220c67835fc7ce8e0e051056546b21
Tim Worrall <tlw72@cornell.edu>
2014-06-26 21:05:30 +0000

    updated to keep in sync with Vitro change

--- src/edu/cornell/mannlib/vitro/webapp/controller/authenticate/LoginRedirector.java
+++ src/edu/cornell/mannlib/vitro/webapp/controller/authenticate/LoginRedirector.java
@@ -156,7 +156,7 @@ public class LoginRedirector {
 
 	private boolean canSeeSiteAdminPage() {
 		return PolicyHelper.isAuthorizedForActions(request,
-				SimplePermission.SEE_SITE_ADMIN_PAGE.ACTIONS);
+				SimplePermission.SEE_SITE_ADMIN_PAGE.ACTION);
 	}
 
 	private boolean isLoginPage(String page) {
