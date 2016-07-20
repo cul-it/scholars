a7c8e37394997daf36dbd0f17b5e30c0424f9f3d
Jim Blake <jeb228@cornell.edu>
2016-07-20 14:37:40 -0400

    Revert "Move Vitro changes to Scholars-Vitro"
    This reverts commit 2a20997c545af02b08e92394e4fc7150fdb1c8e2.

---
+++ src/edu/cornell/mannlib/vitro/webapp/controller/api/distribute/PersonWordCloudDistributor.java
@@ -0,0 +1,90 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.mannlib.vitro.webapp.controller.api.distribute;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.rdf.model.Model;
+
+import edu.cornell.mannlib.vitro.webapp.modelaccess.RequestModelAccess;
+
+/**
+ * An interim implementation -- this should be replaced by a generalized
+ * CONSTRUCT distributor.
+ */
+public class PersonWordCloudDistributor extends RdfDataDistributorBase {
+	private static final Log log = LogFactory
+			.getLog(PersonWordCloudDistributor.class);
+
+	/** The models on the current request. */
+	private RequestModelAccess models;
+
+	/** The construct query to be executed. */
+	private static final String QUERY_STRING = "" //
+			+ "PREFIX rdf:      <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n " //
+			+ "PREFIX rdfs:     <http://www.w3.org/2000/01/rdf-schema#> \n " //
+			+ "PREFIX bibo:     <http://purl.org/ontology/bibo/> \n " //
+			+ "PREFIX vivo:     <http://vivoweb.org/ontology/core#> \n " //
+			+ "CONSTRUCT { \n " //
+			+ "  ?pub rdfs:label ?label . \n " //
+			+ "  ?pub vivo:freetextKeyword ?keyword . \n " //
+			+ "} \n " //
+			+ "WHERE \n " //
+			+ "{ \n " //
+			+ "  ?person vivo:relatedBy ?auth . \n " //
+			+ "  ?auth a vivo:Authorship . \n " //
+			+ "  ?auth vivo:relates ?pub . \n " //
+			+ "  ?pub a bibo:Document . \n " //
+			+ "  ?pub rdfs:label ?label . \n " //
+			+ "  ?pub vivo:freetextKeyword ?keyword . \n " //
+			+ "}";
+
+	@Override
+	public void init(DataDistributorContext ddContext) {
+		super.init(ddContext);
+		this.models = ddContext.getRequestModels();
+	}
+
+	@Override
+	public Model execute() throws DataDistributorException {
+		String uri = getPersonUri();
+		String queryStr = QUERY_STRING.replace("?person", "<" + uri + ">");
+
+		OntModel model = models.getOntModel();
+		QueryExecution qe = null;
+		try {
+			Query query = QueryFactory.create(queryStr, Syntax.syntaxARQ);
+			qe = QueryExecutionFactory.create(query, model);
+			return qe.execConstruct();
+		} catch (Exception e) {
+			throw new ActionFailedException(e);
+		} finally {
+			if (qe != null) {
+				qe.close();
+			}
+		}
+	}
+
+	private String getPersonUri() throws MissingParametersException {
+		String[] persons = parameters.get("person");
+		if (persons == null || persons.length == 0) {
+			throw new MissingParametersException(
+					"A 'person' parameter is required.");
+		} else {
+			return persons[0];
+		}
+	}
+
+	@Override
+	public void close() throws DataDistributorException {
+		// Nothing to do.
+	}
+
+}
---
+++ src/edu/cornell/mannlib/vitro/webapp/controller/api/distribute/modelbuilder/ConstructModelBuilder.java
@@ -0,0 +1,106 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.mannlib.vitro.webapp.controller.api.distribute.modelbuilder;
+
+import static edu.cornell.mannlib.vitro.webapp.utils.sparqlrunner.SparqlQueryRunner.createConstructQueryContext;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+import edu.cornell.mannlib.vitro.webapp.controller.api.distribute.DataDistributor.DataDistributorException;
+import edu.cornell.mannlib.vitro.webapp.controller.api.distribute.DataDistributor.MissingParametersException;
+import edu.cornell.mannlib.vitro.webapp.controller.api.distribute.DataDistributorContext;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.RequestModelAccess;
+import edu.cornell.mannlib.vitro.webapp.utils.configuration.Property;
+import edu.cornell.mannlib.vitro.webapp.utils.sparqlrunner.SparqlQueryRunner.ConstructQueryContext;
+
+/**
+ * Run a construct query to build the model. Bind parameters from the request, as needed.
+ */
+public class ConstructModelBuilder implements ModelBuilder {
+	private static final Log log = LogFactory
+			.getLog(ConstructModelBuilder.class);
+
+	protected RequestModelAccess models;
+	private Map<String, String[]> parameters;
+	protected String rawConstructQuery;
+	private Set<String> uriBinders = new HashSet<>();
+	private Set<String> literalBinders = new HashSet<>();
+
+	@Property(uri = "http://vitro.mannlib.cornell.edu/ns/vitro/ApplicationSetup#uriBinding")
+	public void addUriBinder(String uriBindingName) {
+		this.uriBinders.add(uriBindingName);
+	}
+
+	@Property(uri = "http://vitro.mannlib.cornell.edu/ns/vitro/ApplicationSetup#literalBinding")
+	public void addLiteralBinder(String literalBindingName) {
+		this.literalBinders.add(literalBindingName);
+	}
+
+	@Property(uri = "http://vitro.mannlib.cornell.edu/ns/vitro/ApplicationSetup#constructQuery", minOccurs = 1, maxOccurs = 1)
+	public void setRawConstructQuery(String query) {
+		rawConstructQuery = query;
+	}
+
+	@Override
+	public void init(DataDistributorContext ddContext)
+			throws DataDistributorException {
+		this.parameters = ddContext.getRequestParameters();
+		this.models = ddContext.getRequestModels();
+	}
+
+	@Override
+	public Model buildModel() throws DataDistributorException {
+		ConstructQueryContext queryContext = createConstructQueryContext(
+				models.getRDFService(), rawConstructQuery);
+		queryContext = bindUriParameters(queryContext);
+		queryContext = bindLiteralParameters(queryContext);
+		log.debug("Query context is: " + queryContext);
+		return queryContext.execute().toModel();
+	}
+
+	protected ConstructQueryContext bindUriParameters(
+			ConstructQueryContext queryContext)
+			throws MissingParametersException {
+		for (String name : this.uriBinders) {
+			queryContext = queryContext.bindVariableToUri(name,
+					getOneParameter(name));
+		}
+		return queryContext;
+	}
+
+	protected ConstructQueryContext bindLiteralParameters(
+			ConstructQueryContext queryContext)
+			throws MissingParametersException {
+		for (String name : this.literalBinders) {
+			queryContext = queryContext.bindVariableToPlainLiteral(name,
+					getOneParameter(name));
+		}
+		return queryContext;
+	}
+
+	private String getOneParameter(String name)
+			throws MissingParametersException {
+		String[] uris = parameters.get(name);
+		if (uris == null || uris.length == 0) {
+			throw new MissingParametersException("A '" + name
+					+ "' parameter is required.");
+		} else if (uris.length > 1) {
+			throw new MissingParametersException(
+					"Unexpected multiple values for '" + name + "' parameter.");
+		}
+		return uris[0];
+	}
+
+	@Override
+	public void close() {
+		// Nothing to do.
+	}
+
+}
