7ccc387254870eb4e6afba444f886feceaf02b40
Jim Blake <jeb228@cornell.edu>
2016-11-16 12:26:44 -0500

    Person word-cloud - make it case independent.
    SCHELE-190

--- productMods/js/scholars-vis/wordcloud/person-word-cloud.js
+++ productMods/js/scholars-vis/wordcloud/person-word-cloud.js
@@ -14,54 +14,103 @@ function close_word_cloud(target) {
 	$('div.d3-tip').remove();
 }
 
+/**
+ * Transform the RDF graph into the JSON structure that is expected by draw_word_cloud
+ * 
+ * Here is an example of the output:
+ * [
+ *   {
+ *     "text": "prostate cancer",
+ *     "size": 2,
+ *     "entities": [
+ *       {
+ *         "uri": "/scholars/display/UR-380249",
+ *         "text": "E-selectin ligand-1 controls circulating prostate cancer cell rolling/adhesion and metastasis"
+ *       },
+ *       {
+ *         "uri": "/scholars/display/UR-19472",
+ *         "text": "Human fucosyltransferase 6 enables prostate cancer metastasis to bone"
+ *       }
+ *     ]
+ *   }
+ * ]
+ * 
+ * The transformation ensures that:
+ * -- Two keywords are considered the same if the only difference is capitalization.
+ * -- If an article lists two equivalent keywords (differ only in case), only one is counted.
+ * -- On display, the first letter of each keyword is capitalized.
+ * -- If an article has no label, it is ignored.
+ */
 function transform_word_cloud_data(graph) {
 	var VIVO = $rdf.Namespace("http://vivoweb.org/ontology/core#");
 	var RDFS = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#");
 
-	var keywordSet = buildKeywordSet();
-	var keywordArray = populateKeywordArray(keywordSet);
-	return sortAndSlice(keywordArray);
+	var jsonResult = [];
+	var stmts = graph.statementsMatching(undefined, VIVO('freetextKeyword'));
+	stmts.forEach(processStatement);
+	return sortAndSlice(jsonResult);
 
-	function buildKeywordSet() {
-		var stmts = graph
-				.statementsMatching(undefined, VIVO('freetextKeyword'));
-		return new Set(stmts.map(getObjectValue));
+	function processStatement(statement) {
+		var articleUri = statement.subject.uri;
+		var keyword = statement.object.value;
+		var bucket = getBucket(keyword);
+		addToBucket(articleUri, bucket);
+		
+		function getBucket(keyword) {
+			return findBucket(keyword) || createBucket(keyword);
+			
+			function findBucket(keyword) {
+				return jsonResult.find(bucketMatcher);
+				
+				function bucketMatcher(bucket) {
+					return toMatcher(keyword) === bucket.matcher;
+				}
+			}
+			
+			function createBucket(keyword) {
+				var bucket = {
+						matcher: toMatcher(keyword),
+						text: toDisplay(keyword),
+						size: 0,
+						entities: []
+				};
+				jsonResult.push(bucket);
+				return bucket;
+				
+				function toDisplay(keyword) {
+					return keyword.charAt(0).toUpperCase() + keyword.slice(1);
+				}
+			}
+			
+			function toMatcher(keyword) {
+				return keyword.toLowerCase();
+			}
+		}
 
-		function getObjectValue(stmt) {
-			return stmt.object.value
+		function addToBucket(uri, bucket) {
+			var label = graph.any($rdf.sym(uri), RDFS("label"));
+			if (label && label.value) {
+				addToEntities(label.value);
+			}
+			
+			function addToEntities(labelText) {
+				if (! bucket.entities.find(findUri)) {
+					bucket.entities.push({uri: uri, text: labelText});
+					bucket.size = bucket.size + 1;
+				}
+				
+				function findUri(entity) {
+					return entity.uri === uri;
+				}
+			}
 		}
 	}
-
-	function populateKeywordArray(keywordSet) {
-		return Array.from(keywordSet).map(gatherKeywordInfo)
-	}
-
- 	function gatherKeywordInfo(keyword) {
- 		var stmts = graph.statementsMatching(undefined, undefined, keyword);
- 		return {
- 			text : keyword,
- 			size : stmts.length,
- 			entities : stmts.map(getEntityInfo).filter(hasLabel)
- 		}
-
- 		function getEntityInfo(stmt) {
- 			var info = {uri : toDisplayPageUrl(stmt.subject.uri)};
- 			var label = graph.any(stmt.subject, RDFS("label"));
- 			if (label) {
- 				info["text"] = label.value;
- 			}
- 			return info;
- 		}
- 		function hasLabel(entity) {
- 			return entity.text && entity.text.length > 0 
- 		}
-	}
-
-	function sortAndSlice(keywordArray) {
-		return keywordArray.sort(compareSizes).slice(0, 50);
-
-		function compareSizes(a, b) {
-			return b.size - a.size;
+	
+	 function sortAndSlice(jsonResult) {
+		 return jsonResult.sort(compareSizes).slice(0, 50);
+	
+		 function compareSizes(a, b) {
+			 return b.size - a.size;
 		}
 	}
 }
